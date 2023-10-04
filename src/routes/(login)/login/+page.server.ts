99178848a505738d9dddcefe5fa936fd942c9886
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-04 14:26:45 +0100

    TK: Big up myself

--- src/routes/(login)/login/+page.server.ts
+++ src/routes/(login)/login/+page.server.ts
@@ -1,33 +1,21 @@
 import { auth } from '$lib/firebase';
+import { redirect } from '@sveltejs/kit';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-    login: async ({request}) => {
+    default: async ({ request }) => {
         const data = await request.formData();
-		const email = data.get('email') as string;
-		const password = data.get('password') as string;
+        const email = data.get('email') as string;
+        const password = data.get('password') as string;
         if (email != null && password != null) {
             createUserWithEmailAndPassword(auth, email, password)
-            .then(() => {
-                
-            })
-            .catch(() => {
-            });
+                .then(() => {
+                    throw redirect(300, "/");
+                })
+                .catch(() => {
+                });
         }
     },
-    logout: async ({request}) => {
-        const data = await request.formData();
-		const email = data.get('email') as string;
-		const password = data.get('password') as string;
-        if (email != null && password != null) {
-            createUserWithEmailAndPassword(auth, email, password)
-            .then(() => {
-                
-            })
-            .catch(() => {
-            });
-        }
-    }
 };
--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -1,21 +1,9 @@
 <script>
 	import { auth } from '$lib/firebase';
 	import { createUserWithEmailAndPassword } from 'firebase/auth';
-
-	let createUser = createUserWithEmailAndPassword(auth, "tarun", "1234")
-		.then((userCredential) => {
-			// Signed up
-			const user = userCredential.user;
-			// ...
-		})
-		.catch((error) => {
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			// ..
-		});
 </script>
 
-<form method="POST" action="?/login">
+<form method="POST">
 	<label>
 		Email
 		<input name="email" type="email">
