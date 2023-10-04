d37bc24af582cd27cb34f0587364fd9ad2ab0261
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-04 17:28:14 +0100

    TK: Got signup form working

--- src/routes/(login)/login/+page.server.ts
+++ src/routes/(login)/login/+page.server.ts
@@ -1,22 +1,38 @@
 import { auth } from '$lib/firebase';
 import { redirect } from '@sveltejs/kit';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import type { FirebaseError } from 'firebase/app';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-    default: async ({ request }) => {
+    signUp: async ({ request }) => {
+        const data = await request.formData();
+        const email = data.get('email') as string;
+        const password = data.get('password') as string;
+        let success = false;
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            success = true;
+        } catch (error) {
+            return {error: error.message};
+        }
+        if (success) {
+            throw redirect(303, '/');
+        }
+    },
+    logIn: async ({ request }) => {
         const data = await request.formData();
         const email = data.get('email') as string;
         const password = data.get('password') as string;
         if (email != null && password != null) {
-            createUserWithEmailAndPassword(auth, email, password)
+            signInWithEmailAndPassword(auth, email, password)
                 .then(() => {
-                    console.log()
+                    throw redirect(303, "/");
                 })
-                .catch(() => {
+                .catch(({ error }) => {
+                    return { error: error.message };
                 });
         }
-        throw redirect(303, "/");
     },
 };
--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -1,13 +1,34 @@
-<script></script>
+<script>
+    import {enhance} from "$app/forms";
 
-<form method="POST">
+    /** @type {import('./$types').ActionData} */
+    export let form;
+
+    $: message = "Is there an error: " + form?.error;
+</script>
+
+<form method="POST" action="?/signUp" use:enhance>
 	<label>
 		Email
-		<input name="email" type="email">
+		<input name="email" type="email"/>
 	</label>
 	<label>
 		Password
-		<input name="password" type="password">
+		<input name="password" type="password" />
 	</label>
-	<button>Log in</button>
+	<button>Sign Up</button>
+</form>
+
+<div>{message}</div>
+
+<form method="POST" action="?/logIn">
+	<label>
+		Email
+		<input name="email" type="email" />
+	</label>
+	<label>
+		Password
+		<input name="password" type="password" />
+	</label>
+	<button>Log In</button>
 </form>
