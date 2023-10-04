7f8818e0389392ca2a5e4bcff5264e7902e29491
paolosait <paolosait7@gmail.com>
2023-10-04 12:50:29 +0100

    Added new post logic

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -1,4 +1,7 @@
 import type { User } from "./user";
+import { collection, addDoc } from "firebase/firestore"; 
+import { db } from "./firebase";
+
 
 export interface Post {
     postId: string;
@@ -13,4 +16,17 @@ interface PostComment {
     commentor: User;
     text: string;
     date: Date;
+}
+
+export async function NewPost() {
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+            first: "Ada",
+            last: "Lovelace",
+            born: 1815
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
 }
