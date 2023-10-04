ac3f9489068f4900dafc54b9498a138507148837
paolosait <paolosait7@gmail.com>
2023-10-04 11:44:11 +0100

    Started building post widget

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
+<script>
+    import PostWidget from "./PostWidget.svelte";
+</script>
+
 <div>
-	<h1>Project Broteas</h1>
-	<p>
-		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation alsidflasjdfljasdlfjalsdkjflaskdflaskdf
-	</p>
+	<h1>Project Broteas HOME</h1>
+    <PostWidget user="Mike"/>
 </div>
---
+++ src/routes/PostWidget.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let user: String;
+</script>
+
+
+<div class="w-64 h-64 p-2 bg-gray-200 flex-col">
+    <div>
+        Song Name - Artist Name
+    </div>
+    <div>
+        Shared by {user}
+    </div>
+</div>
