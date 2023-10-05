d801b5e6d7ffcd6383d649494b0ed23fb3371bc7
paolosait <paolosait7@gmail.com>
2023-10-05 18:21:19 +0100

    small visual changes

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -21,15 +21,18 @@
 	});
 </script>
 
-<div class="bg-gray-100 grow">
-	<h1>Project Broteas HOME</h1>
-
-	<button on:click={() => (showModal = true)}
-		><svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"
+<div class="bg-gray-50 grow flex-col flex items-center">
+	<button on:click={() => (showModal = true)} class="fixed right-4 bottom-4"
+		>
+        <div class="bg-green-100 w-14 h-14 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"
 			><path
 				d="M186.666-120q-27 0-46.833-19.833T120-186.666v-586.668q0-27 19.833-46.833T186.666-840h382.668v66.666H186.666v586.668h586.668v-382.668H840v382.668q0 27-19.833 46.833T773.334-120H186.666Zm134.001-158v-66.666H640V-278H320.667Zm0-124.667v-66.666H640v66.666H320.667Zm0-124.667V-594H640v66.666H320.667ZM688-602.667V-688h-85.333v-66.667H688V-840h66.667v85.333H840V-688h-85.333v85.333H688Z"
 			/></svg
-		></button
+		>
+        </div>
+        
+        </button
 	>
 
 	<Modal bind:showModal>
--- src/routes/(app)/components/PostWidget.svelte
+++ src/routes/(app)/components/PostWidget.svelte
@@ -6,30 +6,30 @@
 <div class="w-96 p-2 bg-white flex-col">
 	<div class="flex-row flex">
 		<div class=" w-10 h-10 rounded-full bg-green-200" />
-        <div class="w-2"></div>
+		<div class="w-2" />
 		<div class="flex-col flex">
 			<div>{post.creatorId}</div>
 			<div class="text-xs">{post.date}</div>
 		</div>
 	</div>
-<div class="h-4"></div>
-	<div class="w-80 h-80 bg-gray-100">
-		Object id is {post.objectId}
+	<div class="h-4" />
+	<div class="flex-row flex">
+		<div class="w-80 h-80 bg-gray-100 rounded-lg">
+			Object id is {post.objectId}
+		</div>
+		<div class="flex-col flex justify-center items-center">
+			<div>like</div>
+			<div>{post.likes.length}</div>
+			<div class="h-4" />
+			<div>comment</div>
+			<div>{post.comments.length}</div>
+            <div class="h-4" />
+			<div>share</div>
+		</div>
 	</div>
-    <div class="h-4"></div>
+	<div class="h-4" />
 	<div class="font-medium">
 		{post.text}
 	</div>
-	<div>
-		Shared by user {post.creatorId}
-	</div>
-	<div class="flex-row flex">
-		<div>
-			Likes: {post.likes.length}
-		</div>
-		<div class="w-4" />
-		<div>
-			Comments: {post.comments.length}
-		</div>
-	</div>
+	<div class="h-2" />
 </div>
