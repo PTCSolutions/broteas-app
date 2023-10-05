ad5b86cc5dccc9e0efb2c0d4c7c4b7b4131cc361
paolosait <paolosait7@gmail.com>
2023-10-05 22:24:16 +0100

    Make liking a toggle

--- src/routes/(app)/api/like/+server.ts
+++ src/routes/(app)/api/like/+server.ts
@@ -1,17 +1,29 @@
-import { doc, updateDoc } from "firebase/firestore";
+import { arrayUnion, doc, updateDoc, DocumentSnapshot, getDoc, arrayRemove } from "firebase/firestore";
 import { db } from "$lib/firebase.js";
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ url, cookies }): Promise<void> {
+export async function GET({ url, cookies }) {
     const uid = cookies.get('uid');
     const postId: string | null = url.searchParams.get('postId');
-    // const querySnapshot = await getDoc(doc(db, "posts", "ref?"));
-    //TODO: Make this add to the likes not rewrite them
-    if (postId != null) {
-        await updateDoc(doc(db, "posts", postId), {
-            likes: [uid]
-        });
+    if (postId != null && uid != null) {
+
+        const docSnapshot: DocumentSnapshot = await getDoc(doc(db, "posts", postId));
+        if (docSnapshot.exists()) {
+            const likes: Array<string> = docSnapshot.data().likes;
+
+            if (likes.includes(uid)) {
+                await updateDoc(doc(db, "posts", postId), {
+                    likes: arrayRemove(uid)
+                });
+            } else {
+                await updateDoc(doc(db, "posts", postId), {
+                    likes: arrayUnion(uid)
+                });
+            }
+
+        }
+
     }
     throw redirect(303, "/");
 }
