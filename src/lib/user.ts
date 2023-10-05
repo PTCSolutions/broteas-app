03dddb07049e5914231ae2cfde16bbe0ffce437f
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-05 18:25:07 +0100

    TK: Update profile page

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,4 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, DocumentSnapshot } from "firebase/firestore";
 import { db } from "./firebase";
 
 export interface User {
@@ -24,4 +24,21 @@ export async function newUser(user: User) {
     } catch (e) {
         console.error("Error adding document: ", e);
     }
+}
+
+export async function getUser(uid: string) : Promise<User | null> {
+    const docSnapshot: DocumentSnapshot = await getDoc(doc(db, "users", uid));
+    const data = docSnapshot.data();
+    if (data != undefined) {
+        const user : User = {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email,
+            following: data.following,
+            followers: data.followers,
+            uid: uid
+        };
+        return user;
+    }
+    return null;
 }
--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -1,1 +1,17 @@
-<h1>SETTINGS</h1>
+<script lang="ts">
+    import type {User} from "$lib/user";
+    // Get User from load page
+    export let data : User | null;
+</script>
+
+<div class="flex flex-col">
+    <h1>SETTINGS</h1>
+    <h2>Your profile:</h2>
+    {#if data != null}
+    <div>{`Uid: ${data?.uid}`}</div>
+    <div>{`First Name: ${data?.firstName}`}</div>
+    <div>{`Last Name: ${data?.lastName}`}</div>
+    <div>{`Followers: ${data?.followers.length}`}</div>
+    <div>{`Following: ${data?.following.length}`}</div>
+    {/if}
+</div>
