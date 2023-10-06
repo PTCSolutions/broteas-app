261fcb9a6e4e077ea305e29d9614493f58575eb2
paolosait <paolosait7@gmail.com>
2023-10-06 16:02:31 +0100

    Moved new post widget to own component

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,6 +2,7 @@
 	import PostWidget from './components/PostWidget.svelte';
 	import { GetPosts } from '$lib/post';
 	import Modal from './components/Modal.svelte';
+    import NewPostWidget from './components/NewPostWidget.svelte';
 	export let data;
     let accessToken: string = data?.accessToken;
 	let showModal = false;
@@ -19,34 +20,7 @@
 	</button>
 
 	<Modal bind:showModal>
-		<div class="w-128 mt-4 rounded-sm p-8 bg-gray-200">
-			<form class="my-4" method="POST">
-				<div>
-					<label class="label text-slate-500 mr-4" for="name">Text </label>
-					<input
-						type="text"
-						name="text"
-						autocomplete="off"
-						placeholder="Type here"
-						class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full"
-					/>
-				</div>
-				<div>
-					<label class="label text-slate-500 mr-4" for="name">ID </label>
-					<input
-						type="text"
-						name="objectId"
-						autocomplete="off"
-						placeholder="Type here"
-						class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full"
-					/>
-				</div>
-				<div class="h-2" />
-				<div>
-					<button>Post</button>
-				</div>
-			</form>
-		</div>
+       <NewPostWidget />
 	</Modal>
 
 	<div class="text-lg">Latest Posts:</div>
---
+++ src/routes/(app)/components/NewPostWidget.svelte
@@ -0,0 +1,28 @@
+<div class="w-128 mt-4 rounded-sm p-8 bg-gray-200">
+			<form class="my-4" method="POST" action="/">
+				<div>
+					<label class="label text-slate-500 mr-4" for="name">Text </label>
+					<input
+						type="text"
+						name="text"
+						autocomplete="off"
+						placeholder="Type here"
+						class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full"
+					/>
+				</div>
+				<div>
+					<label class="label text-slate-500 mr-4" for="name">ID </label>
+					<input
+						type="text"
+						name="objectId"
+						autocomplete="off"
+						placeholder="Type here"
+						class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full"
+					/>
+				</div>
+				<div class="h-2" />
+				<div>
+					<button>Post</button>
+				</div>
+			</form>
+		</div>
