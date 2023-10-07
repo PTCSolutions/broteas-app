86384cc198ad3858c1b397ebb89c77f4e8b78873
paolosait <paolosait7@gmail.com>
2023-10-07 15:16:31 +0100

    Fixed some layout issues

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -18,8 +18,9 @@
 
 <NavBar />
 <div class="flex-row flex">
-	<div class="w-32" />
-	<div class="w-6"></div>
+	<div class="w-32 m-2" />
+	<div class="w-2"></div>
+
 	<div class="grow">
 		<slot />
 	</div>
