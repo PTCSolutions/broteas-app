883180a869e4a2d4e87e845d2616f5afdd26f78a
paolosait <paolosait7@gmail.com>
2023-10-09 23:18:41 +0100

    Non working artist page

---
+++ src/routes/(app)/artists/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+	import { getContext } from 'svelte';
+
+	let json;
+    
+    const response = await fetch(`https://api.spotify.com/v1/artists/${data.artistId}`, {
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${accessToken}`
+				}
+			});
+			if (response.status == 200) {
+				let json = await response.json();
+				
+			} else {
+
+			}
+</script>
+
+<div>
+    <h1>{data.artistId}</h1>
+	<p>{json.name}</p>
+</div>
