2b8e1e6a279eab03ac40f32a8e1773682bd2f692
paolosait <paolosait7@gmail.com>
2023-10-13 00:46:24 +0100

    Updated weird signup and login

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -6,17 +6,26 @@
 	export let form;
 </script>
 
-<!-- Use progressive enhancement so that we can get nice transitions if js enabled-->
-<form method="POST" use:enhance>
-	<div class="flex flex-col pl-5 w-1/2 gap-2 pt-5">
-		<Input id="email" type="email" placeholder="Email" />
-		<Input id="password" type="password" placeholder="Password" />
-		<div class="w-1/6"><Button text="Log In"/></div>
-	</div>
-	
-	
-</form>
-<!-- If there is an error on submitting the form , show it in red underneath-->
-{#if form?.error != undefined}
-	<div class="text-red-500">{form.error}</div>
-{/if}
+<div class="h-full">
+	<img src="/WeirdSofa.png" class="z-0 h-full overflow-hidden min-h-screen object-cover" alt="" />
+	<!-- Use progressive enhancement so that we can get nice transitions if js enabled-->
+	<form class="z-10 fixed right-0 top-0 w-1/2 p-8  h-full" method="POST" use:enhance>
+		<!-- TODO: Make this use form validation instead of html validation-->
+		<div class="p-16 w-full gap-6 flex flex-col h-full justify-center">
+			<div class="text-6xl font-bold">Login</div>
+			<div>
+				<Input id="email" type="email" placeholder="Email" />
+			</div>
+			<div>
+				<Input id="password" type="password" placeholder="Password" />
+			</div>
+
+			<div class="w-full"><Button text="Log In" /></div>
+		</div>
+	</form>
+	<!-- If there is an error on submitting the form, show it in red underneath-->
+	{#if form?.error != undefined}
+		<!-- TODO: Have used normal css because cant get tailwind to work-->
+		<div class="text-red-500">{form.error}</div>
+	{/if}
+</div>
--- src/routes/(login)/signup/+page.svelte
+++ src/routes/(login)/signup/+page.svelte
@@ -6,14 +6,13 @@
 	export let form;
 </script>
 
-<div class="h-full">
-	<img src="/Weird Background.png" class="z-0 h-full overflow-hidden min-h-screen object-cover" alt="" />
+<div class="h-full overflow-hidden">
+	<img src="/WeirdTree.png" class="z-0 h-full overflow-hidden min-h-screen object-cover" alt="" />
 	<!-- Use progressive enhancement so that we can get nice transitions if js enabled-->
 	<form class="z-10 fixed right-0 top-0 w-1/2 p-8  h-full" method="POST" use:enhance>
 		<!-- TODO: Make this use form validation instead of html validation-->
-		<div class="p-16 w-full gap-6 flex flex-col h-full">
+		<div class="p-16 w-full gap-6 flex flex-col h-full justify-center">
 			<div class="text-6xl font-bold">Signup now</div>
-			<div class="h-1"></div>
 			<div class="w-full">
 				<Input id="firstName" type="text" placeholder="First Name" required={true} />
 			</div>
@@ -27,7 +26,7 @@
 				<Input id="password" type="password" placeholder="Password" />
 			</div>
 
-			<div class="w-full"><Button text="Log In" /></div>
+			<div class="w-full"><Button text="Join" /></div>
 		</div>
 	</form>
 	<!-- If there is an error on submitting the form, show it in red underneath-->
