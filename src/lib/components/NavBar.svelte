f103257f5a4852b9fb1569db0859947ba670286e
paolosait <paolosait7@gmail.com>
2023-10-12 18:15:55 +0100

    Moved new post to navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,6 +1,10 @@
-<script>
+<script lang='ts'>
 	import MenuItem from './MenuItem.svelte';
 	import { userProfileStore } from '$lib/stores/userStore';
+	import Modal from './Modal.svelte';
+	import NewPost from './NewPost.svelte';
+
+	let showModal: Boolean = false;
 </script>
 
 <aside class="flex-col flex w-32 bg-gray-50 min-h-screen p-2 border-r-2 fixed">
@@ -12,10 +16,11 @@
 		<MenuItem title="Home" href="/" />
 		<MenuItem title="News" href="/news" />
 		<MenuItem title="Search" href="/search" />
+		<button on:click={() => (showModal = true)} class="my-1 py-1 px-2 rounded-md hover:bg-gray-300 text-xl text-left">New Post</button>
 	</div>
 
 	<div class="grow" />
-	<MenuItem title="Settings" href="/settings" small={true}/>
+	<MenuItem title="Settings" href="/settings" small={true} />
 
 	{#if $userProfileStore?.loading}
 		<div />
@@ -31,3 +36,7 @@
 		<!-- Otherwise show empty row-->
 	{/if}
 </aside>
+
+<Modal bind:showModal>
+	<NewPost />
+</Modal>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -10,19 +10,6 @@
 </script>
 
 <div class="bg-gray-50 flex-col flex items-start">
-	<button on:click={() => (showModal = true)} class="fixed right-4 bottom-4">
-		<div class="bg-green-100 w-14 h-14 rounded-full">
-			<svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"
-				><path
-					d="M186.666-120q-27 0-46.833-19.833T120-186.666v-586.668q0-27 19.833-46.833T186.666-840h382.668v66.666H186.666v586.668h586.668v-382.668H840v382.668q0 27-19.833 46.833T773.334-120H186.666Zm134.001-158v-66.666H640V-278H320.667Zm0-124.667v-66.666H640v66.666H320.667Zm0-124.667V-594H640v66.666H320.667ZM688-602.667V-688h-85.333v-66.667H688V-840h66.667v85.333H840V-688h-85.333v85.333H688Z"
-				/></svg
-			>
-		</div>
-	</button>
-
-	<Modal bind:showModal>
-		<NewPost />
-	</Modal>
 	<!-- The main section of the page is a grid of 3 columns. Each item is a post widget-->
 	<div class="text-2xl font-semibold mt-4 mx-4">Welcome back {name}</div>
 	<div class="grid grid-cols-3 w-full p-4 justify-between gap-4">
