d23ce077176f1ae6f453fb2e913ed97695b2c592
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-13 16:12:18 +0100

    TK: Fixed bug for access_token

--- src/routes/(app)/artists/[id]/+page.ts
+++ src/routes/(app)/artists/[id]/+page.ts
@@ -1,13 +1,16 @@
 import { getArtistTopTracks, getObjectJson, type Artist, type Song, type Album, getArtistAlbums } from '$lib/spotify.js'
 
-export async function load({ params }) {
-    const artist: Artist = await getObjectJson(params.id, 'artist');
-    const albums: Album[] = await getArtistAlbums(params.id);
-    const songs: Song[] = await getArtistTopTracks(params.id);
-    return {
-        artist: artist,
-        songs: songs,
-        albums: albums
-    };
+export async function load({ params, parent }) {
+    const currentAccessToken = (await parent()).token;
+    if (currentAccessToken != null) {
+        const artist: Artist = await getObjectJson(params.id, currentAccessToken, 'artist');
+        const albums: Album[] = await getArtistAlbums(params.id);
+        const songs: Song[] = await getArtistTopTracks(params.id);
+        return {
+            artist: artist,
+            songs: songs,
+            albums: albums
+        };
+    }
     return null;
 }
--- src/routes/(app)/posts/[id]/+page.ts
+++ src/routes/(app)/posts/[id]/+page.ts
@@ -1,10 +1,8 @@
 import { getPost, type PostMeta } from '$lib/post.js'
 import { getObjectJson, type PostObject } from '$lib/spotify.js';
-import { accessToken } from '$lib/stores/accessTokenStore.js';
-import { get } from 'svelte/store';
 
-export async function load({ params }) {
-    const currentAccessToken = get(accessToken);
+export async function load({ params, parent }) {
+    const currentAccessToken = (await parent()).token;
     if (currentAccessToken != null) {
         const post: PostMeta | null = await getPost(params.id);
         const object: PostObject = await getObjectJson(post!.objectId, currentAccessToken, post!.objectType);
