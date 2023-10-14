bf1d6e3a1fdccf799636a9a00d2d4194a11fac0b
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-15 00:44:11 +0100

    TK: Made user's name fade in on first load. Added loading property for posts store

--- src/lib/stores/postsStore.ts
+++ src/lib/stores/postsStore.ts
@@ -3,18 +3,16 @@ import { db } from '$lib/firebase';
 import { writable, type Writable } from "svelte/store";
 import type { PostMeta } from '$lib/post';
 
-export const postStore: Writable<PostMeta[]> = writable([]);
+
+export const postStore: Writable<{posts: PostMeta[], loading: boolean}> = writable({posts: [], loading: true});
 
 let messagesUnsubscribeCallback;
 
 const subscribeToMessages = async () => {
-
     const querySnapshot = query(collection(db, "posts"), orderBy("date", "desc"));
-    console.log("Top level run")
     messagesUnsubscribeCallback = onSnapshot(
         querySnapshot,
         (q) => {
-            console.log("Snapshot run")
             const posts: PostMeta[] = [];
             q.forEach((doc) => {
                 const post = doc.data();
@@ -26,8 +24,9 @@ const subscribeToMessages = async () => {
             });
             postStore.update(() => {
                 console.log('💥 querySnapshot: New data: ', posts);
-                return [...posts];
+                return {posts: [...posts], loading: false};
             });
+
         },
         (error) => {
             console.warn("Couldn't update snapshot, maybe the user is logged out?", error);
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,16 +2,26 @@
 	import { postStore } from '$lib/stores/postsStore';
 	import PostWidget from '$lib/components/posts/PostWidget.svelte';
 	import { userProfileStore } from '$lib/stores/userStore';
-	let name : string | undefined;
+	import { fade } from 'svelte/transition';
+	let name: string | undefined;
 	$: name = $userProfileStore?.user?.firstName;
 </script>
 
 <div class="flex-col flex items-start">
 	<!-- The main section of the page is a grid of 3 columns. Each item is a post widget-->
-	<div class="text-2xl font-semibold mt-4 mx-4 dark:text-white">Welcome back {name}</div>
-	<div class="grid grid-cols-3 w-full p-4 justify-between gap-4">
-		{#each $postStore as post (post.postId)}
-			<PostWidget {post} />
-		{/each}
+	<div class="text-2xl font-semibold mt-4 mx-4 dark:text-white">
+		Welcome back
+		{#if $userProfileStore?.loading === false}
+			<span in:fade>
+				{name}
+			</span>
+		{/if}
 	</div>
+	{#if $postStore?.loading === false}
+		<div class="grid grid-cols-3 w-full p-4 justify-between gap-4" in:fade>
+			{#each $postStore.posts as post (post.postId)}
+				<PostWidget {post} />
+			{/each}
+		</div>
+	{/if}
 </div>
