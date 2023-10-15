46963fe08cc63bff5b71c7a19b72c46365f66407
paolosait <paolosait7@gmail.com>
2023-10-15 14:18:09 +0100

    Started on accounts settings

--- src/routes/(app)/account/+page.svelte
+++ src/routes/(app)/account/+page.svelte
@@ -1,3 +1,26 @@
-<div>
-    Account settings
+<script lang="ts">
+	import { userProfileStore } from '$lib/stores/userStore';
+	export let form;
+    export let data;
+    let user: User = data;
+
+</script>
+
+<div class="h-full p-4 overflow-hidden flex flex-col gap-4">
+	{#if user}
+        <div class="text-4xl font-semibold">Manage your account</div>
+        <div class="bg-white dark:bg-gray-600 rounded-lg p-4">
+            <div class="text-xl">Profile</div>
+            <div>Firstname: {user.firstName}</div>
+            <div>Lastname: {user.lastName}</div>
+            <div>Email: ??</div>
+            <div>Passoword: ??</div>
+        </div>
+        <div class="bg-white dark:bg-gray-600 rounded-lg p-4">
+            <div class="text-xl">Posts</div>
+            <div>People who can your posts: "People who follow you"</div>
+            <div>People who can see your comments: "Anyone who can see the post"</div>
+            <div>Show suggested posts: "Yes"</div>
+        </div>
+	{/if}
 </div>
