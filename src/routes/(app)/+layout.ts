f5fc1ce878fdd1bd357bea538f912e113f12a3ed
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-15 14:09:57 +0100

    TK: Crude - logged out users can't access app

---
+++ src/routes/(app)/+layout.ts
@@ -0,0 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load({parent}) {
+    const uid = (await parent()).uid;
+    // If its undefined, because it hasnt been set yet, then set it to the empty string
+    // This matches the value we set to, when the user logs out
+    if (uid == "") {
+        throw redirect(302, "/login");
+    };
+}
--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -9,7 +9,7 @@
 <div class="h-full overflow-hidden">
 	<img src="/WeirdSofa.png" class="z-0 h-full overflow-hidden min-h-screen object-cover" alt="" />
 	<!-- Use progressive enhancement so that we can get nice transitions if js enabled-->
-	<form class="z-10 fixed right-0 top-0 w-1/2 p-8  h-full" method="POST" use:enhance>
+	<form class="z-10 fixed right-0 top-0 w-1/2 p-8 h-full" method="POST" use:enhance>
 		<!-- TODO: Make this use form validation instead of html validation-->
 		<div class="p-16 w-full gap-6 flex flex-col h-full justify-center">
 			<div class="text-6xl font-bold">Login</div>
@@ -21,11 +21,14 @@
 			</div>
 
 			<div class="w-full"><Button text="Log In" /></div>
+			<!-- If there is an error on submitting the form, show it in red underneath-->
+			{#if form?.error != undefined}
+				<!-- TODO: Have used normal css because cant get tailwind to work-->
+				<div class="text-red-500">{form.error}</div>
+			{/if}
+			<div>
+				Don't have an account? <a class="text-blue-500 font-medium" href="/signup">Sign up</a>
+			</div>
 		</div>
 	</form>
-	<!-- If there is an error on submitting the form, show it in red underneath-->
-	{#if form?.error != undefined}
-		<!-- TODO: Have used normal css because cant get tailwind to work-->
-		<div class="text-red-500">{form.error}</div>
-	{/if}
 </div>
