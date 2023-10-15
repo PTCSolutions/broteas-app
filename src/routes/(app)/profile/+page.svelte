fd807a53388a6294629d5a0d2bba73d3e0cd4e53
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-15 14:36:53 +0100

    TK: See your own posts

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import PostGrid from '$lib/components/posts/PostGrid.svelte';
 	import ProfileBanner from '$lib/components/user/ProfileBanner.svelte';
+	import { getUsersPosts } from '$lib/post';
 import { userProfileStore } from '$lib/stores/userStore';
 </script>
 
 <div class="h-full p-4 overflow-hidden flex flex-col">
 	{#if $userProfileStore?.user?.uid != '' && $userProfileStore?.user?.uid != undefined && !$userProfileStore?.loading}
 		<ProfileBanner user={$userProfileStore.user} />
+		{#await getUsersPosts($userProfileStore?.user?.uid)}
+			<div />
+		{:then posts}
+			<PostGrid {posts}/>
+		{/await}
 	{/if}
-
 </div>
