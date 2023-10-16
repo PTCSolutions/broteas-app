3678b16e758a596d85c960b8ebcecbf4ddf611f7
paolosait <paolosait7@gmail.com>
2023-10-16 11:35:43 +0100

    Minor visual corrections

--- src/lib/components/MenuItem.svelte
+++ src/lib/components/MenuItem.svelte
@@ -4,6 +4,6 @@
 	export let small: boolean = false;
 </script>
 
-<a {href} class={`my-1 py-1 px-2 rounded-md hover:bg-gray-300 dark:text-white dark:hover:bg-gray-600 ${small ? "text-md" : "text-xl"}`}>
+<a {href} class={`my-1 py-1 px-2 w-full rounded-md hover:bg-gray-300 dark:text-white dark:hover:bg-gray-600 ${small ? "text-md" : "text-xl"}`}>
 	{title}
 </a>
--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import PostGrid from '$lib/components/posts/PostGrid.svelte';
 	import ProfileBanner from '$lib/components/user/ProfileBanner.svelte';
-	import { getPostsForUser } from '$lib/post';
-import { userProfileStore } from '$lib/stores/userStore';
+	import { getUsersPosts } from '$lib/post';
+	import { userProfileStore } from '$lib/stores/userStore';
 </script>
 
 <div class="h-full p-4 overflow-hidden flex flex-col">
 	{#if $userProfileStore?.user?.uid != '' && $userProfileStore?.user?.uid != undefined && !$userProfileStore?.loading}
 		<ProfileBanner user={$userProfileStore.user} />
-		{#await getPostsForUser($userProfileStore?.user?.uid)}
-			<div />
+		{#await getUsersPosts($userProfileStore?.user?.uid)}
+			<div>loading...</div>
 		{:then posts}
-			<PostGrid {posts}/>
+			<PostGrid {posts} />
 		{/await}
 	{/if}
 </div>
