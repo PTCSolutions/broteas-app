3f308fac06415ab4c66abb8f1f32067e48e33a46
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-17 14:44:03 +0100

    TK: Made profile picture only grey if doesnt exist

--- src/lib/components/ProfilePicture.svelte
+++ src/lib/components/ProfilePicture.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import { getProfilePicture } from '$lib/storage';
-
-
-    export let uid: string | null;
-    export let small: boolean = true;
-
+	import { fade } from 'svelte/transition';
+	export let uid: string | null;
+	export let small: boolean = true;
 </script>
 
 {#await getProfilePicture(uid)}
-	<div class={`${small ? "h-10 w-10" : "h-32 w-32"} rounded-full bg-slate-400`} />
+	<div class={`${small ? 'h-10 w-10' : 'h-32 w-32'} rounded-full bg-white`} />
 {:then url}
 	{#if url}
-		<img class={`${small ? "h-10 w-10" : "h-32 w-32"} rounded-full overflow-hidden`} src={url} alt="avatar" />
+		<img
+			class={`${small ? 'h-10 w-10' : 'h-32 w-32'} rounded-full overflow-hidden`}
+			src={url}
+			alt="avatar"
+			in:fade
+		/>
 	{:else}
-		<div class={`${small ? "h-10 w-10" : "h-32 w-32"} rounded-full bg-slate-400`} />
+		<div class={`${small ? 'h-10 w-10' : 'h-32 w-32'} rounded-full bg-slate-400`} />
 	{/if}
+{:catch error}
+	<div class={`${small ? 'h-10 w-10' : 'h-32 w-32'} rounded-full bg-slate-400`} />
 {/await}
