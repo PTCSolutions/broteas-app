f26e788f2f7a04e7a5bf71cd5ed662f3ebc13293
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-17 15:00:05 +0100

    TK: FIxed post widget layout

--- src/lib/components/posts/widgets/PostObjectWidget.svelte
+++ src/lib/components/posts/widgets/PostObjectWidget.svelte
@@ -3,14 +3,22 @@
 	import { asSong, asArtist, asAlbum } from '$lib/spotify';
 	import AlbumWidget from './PostAlbumWidget.svelte';
 	import SongWidget from './PostSongWidget.svelte';
-    import ArtistWidget from './PostArtistWidget.svelte';
+	import ArtistWidget from './PostArtistWidget.svelte';
+	import IntersectionObserver from '../../IntersectionObserver.svelte';
+	import { fade } from 'svelte/transition';
 	export let object: PostObject;
 </script>
 
-{#if object.type == 'track'}
-	<SongWidget song={asSong(object)} />
-{:else if object.type == 'album'}
-	<AlbumWidget album={asAlbum(object)} />
-    {:else if object.type == 'artist'}
-	<ArtistWidget artist={asArtist(object)} />
-{/if}
+<IntersectionObserver once={true} let:intersecting>
+	{#if intersecting}
+	<div in:fade>
+		{#if object.type == 'track'}
+			<SongWidget song={asSong(object)} />
+		{:else if object.type == 'album'}
+			<AlbumWidget album={asAlbum(object)} />
+		{:else if object.type == 'artist'}
+			<ArtistWidget artist={asArtist(object)} />
+		{/if}
+	</div>
+	{/if}
+</IntersectionObserver>
