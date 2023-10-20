d5bdbc0159e402a8498cdca2cffa6a07ca83353a
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-20 17:19:03 +0100

    TK: Profile only shows essential info

--- src/routes/(app)/account/+page.svelte
+++ src/routes/(app)/account/+page.svelte
@@ -10,17 +10,17 @@
         <div class="text-4xl font-semibold">Manage your account</div>
         <div class="bg-white dark:bg-gray-600 rounded-lg p-4">
             <div class="text-xl">Profile</div>
-            <div>Firstname: {user.firstName}</div>
-            <div>Lastname: {user.lastName}</div>
+            <div>First Name: {user.firstName}</div>
+            <div>Last Name: {user.lastName}</div>
             <div>Username: {user.username}</div>
             <div>Email: {user.email}</div>
-            <div>Password: ??</div>
+            <!-- <div>Password: ??</div> -->
         </div>
-        <div class="bg-white dark:bg-gray-600 rounded-lg p-4">
+        <!-- <div class="bg-white dark:bg-gray-600 rounded-lg p-4">
             <div class="text-xl">Posts</div>
             <div>People who can your posts: "People who follow you"</div>
             <div>People who can see your comments: "Anyone who can see the post"</div>
             <div>Show suggested posts: "Yes"</div>
-        </div>
+        </div> -->
 	{/if}
 </div>
