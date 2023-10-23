cfa1449b03f7e075e7994eb9103465d2405669a5
paolosait <paolosait7@gmail.com>
2023-10-23 21:21:19 +0100

    Link preview func works with open cors

--- cloud-functions/functions/src/linkPreview.ts
+++ cloud-functions/functions/src/linkPreview.ts
@@ -1,5 +1,9 @@
 import * as functions from "firebase-functions";
 import linkPreviewGenerator = require("link-preview-generator");
+import cors = require("cors")
+
+// TODO: Make this only our website and local host
+const corsFunc = cors({origin: true});
 
 const runtimeOpts = {
   timeoutSeconds: 30,
@@ -8,19 +12,23 @@ const runtimeOpts = {
 
 // A function which takes in a url and returns the meta data of the url
 export const linkPreview = functions
+    .region("europe-west2")
     .runWith(runtimeOpts)
     .https
-    .onRequest((request, response) => {
-      const encodedUrl: string | undefined =
-       request.body.url || request.query.url;
-      if (encodedUrl) {
-        const url = decodeURI(encodedUrl);
+    .onRequest(
+        (request, response) => {
+          corsFunc(request, response, () => {
+            const encodedUrl: string | undefined =
+          request.body.url || request.query.url;
+            if (encodedUrl) {
+              const url = decodeURI(encodedUrl);
 
-        // Code for the preview generator package is here: https://github.com/AndrejGajdos/link-preview-generator
-        linkPreviewGenerator(url)
-            .then((data) => response.send(data))
-            .catch((error) => response.status(500).send(error));
-      } else {
-        response.status(500).send("error");
-      }
-    });
+              // Code for the preview generator package is here: https://github.com/AndrejGajdos/link-preview-generator
+              linkPreviewGenerator(url)
+                  .then((data) => response.send(data))
+                  .catch((error) => response.status(500).send(error));
+            } else {
+              response.status(500).send("Url incorrect");
+            }
+          });
+        });
