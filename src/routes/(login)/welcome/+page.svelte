68255d33673d2b42a8859e3802af84f3a6c16c35
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-25 19:57:22 +0100

    TK: More welcome

--- src/routes/(login)/welcome/+page.svelte
+++ src/routes/(login)/welcome/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
+	import { cubicIn, cubicInOut, elasticIn, linear } from 'svelte/easing';
 	import { fade, fly } from 'svelte/transition';
 	let ready = false;
 	onMount(() => (ready = true));
@@ -14,13 +15,17 @@
 </div> -->
 {#if ready}
 <div class="w-full, min-h-screen background flex flex-col items-center pt-32">
-	<div class="flex flex-col gap-20">
+	<div class="flex flex-col gap-20 w-9/12">
 		<div class="text-6xl font-semibold tracking-widest" in:fade={{ duration: 2000 }}>
 			DISCUSS, DISCOVER, <span class="font-bold text-red-600">DISCO</span>
 		</div>
-		<div class="w-1/2" in:fly={{ y: 200, duration: 2000, delay: 1000 }}>
+		<div class="w-1/2 flex flex-col gap-10" in:fly={{ y: 200, duration: 2000, delay: 1000, easing: cubicInOut }}>
 			<div class="text-4xl leading-snug">
 				Disco is a new platform for communities centred around a love for music
+			</div>
+            <div class="text-2xl leading-none">
+				Share your favourite albums, discuss the songs you love, and learn more about your favourite artists...
+                all on Disco!
 			</div>
 		</div>
 	</div>
