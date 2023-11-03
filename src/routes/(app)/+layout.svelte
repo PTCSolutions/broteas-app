93d627b8e570f2fa6b969fc2374e2bca3b926480
paolosait <paolosait7@gmail.com>
2023-11-03 15:47:14 +0000

    Stopped icon alt text loading thing

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,10 +2,6 @@
 	import NavBar from '$lib/components/NavBar.svelte';
 </script>
 
-<link
-	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,100..700,0..1,-50..200"
-	rel="stylesheet"
-/>
 <div class="flex-row flex bg-gray-100 dark:bg-gray-800 dark:text-white min-h-screen">
 	<NavBar />
 	<div class="grow ml-32 h-full">
