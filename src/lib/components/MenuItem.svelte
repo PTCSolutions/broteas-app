a11bbec31cdaffc02a6a9f5aed43b24587a2c6b4
paolosait <paolosait7@gmail.com>
2023-11-18 13:59:30 +0000

    Nv bar shows the page you are on

--- src/lib/components/MenuItem.svelte
+++ src/lib/components/MenuItem.svelte
@@ -3,8 +3,9 @@
 	export let href: string;
 	export let small: boolean = false;
 	export let logout: boolean = false;
+	import { page } from "$app/stores";
 </script>
 
-<a {href} class={`my-1 py-1 px-2 w-full rounded-md hover:bg-gray-300 dark:text-white dark:hover:bg-gray-600 ${small ? "text-md" : "text-xl"} ${logout ? "hover:text-red-600" : ""}`}>
+<a {href} class={`my-1 py-1 px-2 w-full rounded-md hover:bg-gray-300 dark:text-white dark:hover:bg-gray-600 ${small ? "text-md" : "text-xl"} ${logout ? "hover:text-red-600" : ""} ${$page.url.pathname === href ? "font-bold" : ""}`}>
 	{title}
 </a>
